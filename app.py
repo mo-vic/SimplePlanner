7132f8fdd5dc88d9010133ac1833638b20c83733
movic <movic.chen@gmail.com>
2024-06-03 22:09:08 +0800

    add BFS algorithm

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 import sys
 import json
+from queue import Queue
 
 import numpy as np
 
+from PyQt5.QtCore import Qt
 from PyQt5.QtCore import QTimer
 from PyQt5.QtCore import QPointF
 from PyQt5.QtGui import QPolygonF
@@ -20,6 +22,13 @@ from PyQt5.QtWidgets import QApplication
 class GraphicsScene(QGraphicsScene):
     def __init__(self, parent=None):
         super(GraphicsScene, self).__init__(parent=parent)
+
+        self.start = (0, 0)
+        self.goal = (0, 0)
+
+        self.initFinished = False
+
+        self.path = []
 
         with open("canvas.json", 'r') as f:
             json_content = json.load(f)
@@ -54,8 +63,10 @@ class GraphicsScene(QGraphicsScene):
         x_dot_coor = (x_interval[1:] + x_interval[:-1]) / 2.0
         y_dot_coor = (y_interval[1:] + y_interval[:-1]) / 2.0
         self.dots = np.meshgrid(x_dot_coor, y_dot_coor)
+
         self.num_x_coor = len(x_dot_coor)
         self.num_y_coor = len(y_dot_coor)
+        self.grid = np.zeros((self.num_x_coor, self.num_y_coor), dtype=np.int)
 
         # for grid collision detection
         self.blockItem = QGraphicsRectItem()
@@ -63,6 +74,20 @@ class GraphicsScene(QGraphicsScene):
         self.blockHeight = y_interval[1] - y_interval[0]
         self.blockItem.setRect(0, 0, self.blockWidth, self.blockHeight)
         self.addItem(self.blockItem)
+
+        radius = 3
+        diameter = 6
+        self.startItem = QGraphicsEllipseItem()
+        self.startItem.setPen(QPen(QColor(0, 255, 0)))
+        self.startItem.setBrush(QBrush(QColor(0, 255, 0, 255)))
+        self.startItem.setPos(QPointF(0, 0))
+        self.startItem.setRect(self.dots[0][0, 0] - radius, self.dots[1][0, 0] - radius, diameter, diameter)
+
+        self.goalItem = QGraphicsEllipseItem()
+        self.goalItem.setPen(QPen(QColor(255, 0, 0)))
+        self.goalItem.setBrush(QBrush(QColor(255, 0, 0, 255)))
+        self.goalItem.setPos(QPointF(0, 0))
+        self.goalItem.setRect(self.dots[0][0, 0] - radius, self.dots[1][0, 0] - radius, diameter, diameter)
 
     def checkCollision(self, x, y):
         self.blockItem.setX(x)
@@ -78,23 +103,89 @@ class GraphicsScene(QGraphicsScene):
 
         if i >= self.num_y_coor:
             self.removeItem(self.blockItem)
+            self.addItem(self.startItem)
+            self.addItem(self.goalItem)
+            self.initFinished = True
+
             return True
 
-        radius = 3
-        diameter = 6
         x = self.dots[0][i, j]
         y = self.dots[1][i, j]
         isValid = self.checkCollision(x - self.blockWidth / 2.0, y - self.blockHeight / 2.0)
-        dotItem = QGraphicsEllipseItem()
+
         if isValid:
-            dotItem.setPen(QPen(QColor(0, 255, 0)))
-            dotItem.setBrush(QBrush(QColor(0, 255, 0, 255)))
+            self.grid[i, j] = 0
         else:
-            dotItem.setPen(QPen(QColor(255, 0, 0)))
-            dotItem.setBrush(QBrush(QColor(255, 0, 0, 255)))
-        dotItem.setPos(QPointF(0, 0))
-        dotItem.setRect(x - radius, y - radius, diameter, diameter)
-        self.addItem(dotItem)
+            self.grid[i, j] = 1
+
+        return False
+
+    def mouseDoubleClickEvent(self, event):
+        radius = 3
+        diameter = 6
+        x = event.scenePos().x()
+        y = event.scenePos().y()
+
+        i = int(x // self.blockWidth)
+        j = int(y // self.blockHeight)
+        print(event.scenePos().x(), event.scenePos().y())
+        if event.button() == Qt.LeftButton:
+            self.start = (i, j)
+            self.startItem.setRect(self.dots[0][j, i] - radius, self.dots[1][j, i] - radius, diameter, diameter)
+
+        elif event.button() == Qt.RightButton:
+            self.goal = (i, j)
+            self.goalItem.setRect(self.dots[0][j, i] - radius, self.dots[1][j, i] - radius, diameter, diameter)
+
+        print(self.start, self.goal)
+
+        if self.initFinished:
+            print(self.runBFS())
+
+    def runBFS(self):
+        for lineItem in self.path:
+            self.removeItem(lineItem)
+
+        self.path.clear()
+
+        que = Queue()
+        que.put(self.start)
+
+        transition = dict()
+        visited = np.zeros_like(self.grid)
+        visited[self.start[0], self.start[1]] = 1
+        while not que.empty():
+            node = que.get()
+
+            if node == self.goal:
+                traceNode = self.goal
+                while traceNode in transition:
+                    preNode = transition[traceNode]
+                    a, b = traceNode
+                    i, j = preNode
+                    lineItem = QGraphicsLineItem()
+                    lineItem.setPen(QPen(QColor(0, 0, 255), 3))
+                    lineItem.setLine(self.dots[0][b, a], self.dots[1][b, a], self.dots[0][j, i], self.dots[1][j, i])
+                    self.path.append(lineItem)
+                    self.addItem(lineItem)
+                    traceNode = (i, j)
+
+                return True
+
+            a, b = node
+
+            neighbors = [(a - 1, b - 1), (a - 1, b), (a - 1, b + 1),
+                         (a, b - 1), (a, b + 1),
+                         (a + 1, b - 1), (a + 1, b), (a + 1, b + 1)]
+
+            for i, j in neighbors:
+                if 0 <= i < self.num_x_coor and 0 <= j < self.num_y_coor:
+                    if visited[i, j] == 1 or self.grid[j][i] == 1:
+                        continue
+                    else:
+                        que.put((i, j))
+                        visited[i, j] = 1
+                        transition[(i, j)] = (a, b)
 
         return False
 
