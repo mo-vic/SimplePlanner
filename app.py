217416ffac54b93a32d8573859f0e013f14c4373
movic <movic.chen@gmail.com>
2024-06-08 15:37:37 +0800

    print search process

--- app.py
+++ app.py
@@ -266,7 +266,11 @@ class GraphicsScene(QGraphicsScene):
         for lineItem in self.path:
             self.removeItem(lineItem)
 
+        for textItem in self.textItems:
+            self.removeItem(textItem)
+
         self.path.clear()
+        self.textItems.clear()
 
         que = deque()
         que.append(self.start)
@@ -274,8 +278,12 @@ class GraphicsScene(QGraphicsScene):
         transition = dict()
         visited = np.zeros_like(self.grid)
         visited[self.start[0], self.start[1]] = 1
+
+        visitedOrder = 0
         while len(que) > 0:
             node = que.pop()
+
+            visitedOrder += 1
 
             if node == self.goal:
                 self.drawPath(transition)
@@ -283,6 +291,12 @@ class GraphicsScene(QGraphicsScene):
                 return True
 
             a, b = node
+
+            textItem = QGraphicsTextItem()
+            textItem.setPlainText(str(visitedOrder))
+            textItem.setPos(self.dots[0][a, b] - self.blockWidth / 2, self.dots[1][a, b] - self.blockHeight / 2)
+            self.addItem(textItem)
+            self.textItems.append(textItem)
 
             neighbors = [(a - 1, b - 1), (a - 1, b), (a - 1, b + 1),
                          (a, b - 1), (a, b + 1),
