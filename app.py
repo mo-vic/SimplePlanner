5f97a1297eb2a47290a7efabd814d0c217dab514
movic <movic.chen@gmail.com>
2024-06-08 16:19:42 +0800

    print search process

--- app.py
+++ app.py
@@ -429,7 +429,11 @@ class GraphicsScene(QGraphicsScene):
         for lineItem in self.path:
             self.removeItem(lineItem)
 
+        for textItem in self.textItems:
+            self.removeItem(textItem)
+
         self.path.clear()
+        self.textItems.clear()
 
         que = PriorityQueue()
         que.put((0, self.start))
@@ -439,6 +443,8 @@ class GraphicsScene(QGraphicsScene):
         g[self.start] = 0  # length from start point to itself is set to 0
         visited = np.zeros_like(self.grid)
         visited[self.start[0], self.start[1]] = 1
+
+        visitedOrder = 0
         while not que.empty():
             _, node = que.get()
 
@@ -454,6 +460,14 @@ class GraphicsScene(QGraphicsScene):
 
             # move to closed set
             visited[a, b] += 1
+
+            textItem = QGraphicsTextItem()
+            textItem.setPlainText(str(visitedOrder))
+            textItem.setPos(self.dots[0][a, b] - self.blockWidth / 2, self.dots[1][a, b] - self.blockHeight / 2)
+            self.addItem(textItem)
+            self.textItems.append(textItem)
+
+            visitedOrder += 1
 
             # to distinguish between diagonal and axial neighbors
             neighbors = [(a - 1, b - 1, True), (a - 1, b, False), (a - 1, b + 1, True),
